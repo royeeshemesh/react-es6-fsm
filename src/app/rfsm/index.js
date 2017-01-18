13745ad9f25d1e737eda21322c86aa20a050ae97
Royee Shemesh <royee@flexpert.co.il>
2017-01-18 18:25:38 +0200

    Infra/npm prepration (#2)
    * SAVEPOINT
    
    * SAVEPOINT

---
+++ src/app/rfsm/index.js
@@ -0,0 +1,19 @@
+import Action from './Action';
+import ActionState from './ActionState';
+import Event from './Event';
+import State from './State';
+import StateMachine from './StateMachine';
+import Transition from './Transition';
+import View from './View';
+import ViewState from './ViewState';
+
+export {
+  Action,
+  ActionState,
+  Event,
+  State,
+  StateMachine,
+  Transition,
+  View,
+  ViewState
+}
